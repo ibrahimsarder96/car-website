5d0b5baf9a4d71437f3f871b3674227791cf549b
@ibrahimsarder96 <ibrahimsarder969@gmail.com>
2022-04-03 22:52:34 +0600

    added navbar

---
+++ src/components/NotFound/NotFound.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const NotFound = () => {
+  return (
+    <div className='mt-72'>
+      <h1 className='text-xl'>The page you are looking is not available.</h1>
+      <h3 className='text-4xl'>404</h3>
+    </div>
+  );
+};
+
+export default NotFound;
