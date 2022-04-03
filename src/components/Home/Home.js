ef4be180c3ac4d60dce1286b77c148d01464b550
@ibrahimsarder96 <ibrahimsarder969@gmail.com>
2022-04-04 02:07:36 +0600

    reviews added

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,8 +1,11 @@
 import React from 'react';
+import useHomeReview from '../Hookes/useHomeReview';
 
 const Home = () => {
+  const [reviews, setReviews] = useHomeReview();
   return (
-    <div className='md:flex my-48'>
+   <div>
+      <div className='md:flex my-48'>
       <div className='w-6/12 px-4'>
       <h1 className='text-6xl font-semibold text-pink-300'>Find your next car with us</h1>
       <p className='my-10'>There are still some tried-and-true steps you should take when buying a car — as well as a few new ones to consider — to ensure you get the right car for you, at the right price and in a safe manner.</p>
@@ -12,6 +15,12 @@ const Home = () => {
       <img src="https://img.freepik.com/free-vector/blue-sports-car-isolated-white-vector_53876-67418.jpg?size=626&ext=jpg&ga=GA1.2.142737712.1632825724" alt="" />
       </div>
     </div>
+    <section>
+      <div>
+        <h1>{reviews.length}</h1>
+      </div>
+    </section>
+   </div>
   );
 };
 
--- src/components/Reviews/Reviews.js
+++ src/components/Reviews/Reviews.js
@@ -1,9 +1,20 @@
-import React from 'react';
+import React, { useState } from 'react';
+import useHomeReview from '../Hookes/useHomeReview';
+import Review from '../Review/Review';
 
 const Reviews = () => {
+  const [reviews, setReviews] = useHomeReview();
   return (
-    <div>
-      
+    <div className=' mt-8 p-4 bg-indigo-300'>
+      <h1 className='text-4xl font-mono text-white '>Customer Reviews</h1>
+      <div className='grid md:grid-cols-3 gap-3 mt-8'>
+      {
+        reviews.map(review => <Review 
+          key={review.id}
+          review={review}
+          ></Review>)
+      }
+      </div>
     </div>
   );
 };
